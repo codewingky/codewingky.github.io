c06660650b2f3f905a1e0f0f3e5d81c63bf5417c
蒟蒻颖 <zhangying458@126.com>
2022-06-22 23:24:46 +0800

    压缩图片大小，上传一些笔记

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,15 +2,15 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar([
   "/",
-  "/home",
-  "/slide",
-  {
-    text: "如何使用",
-    icon: "creative",
-    prefix: "/guide/",
-    link: "/guide/",
-    children: "structure",
-  },
+  // "/home",
+  // "/slide",
+  // {
+  //   text: "如何使用",
+  //   icon: "creative",
+  //   prefix: "/guide/",
+  //   link: "/guide/",
+  //   children: "structure",
+  // },
   // {
   //   text: "文章",
   //   icon: "note",
